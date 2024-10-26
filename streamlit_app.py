8564ef2c8bc41a0ee4f74d98ce5c7bf03b7c0901
Goyo <goyodiaz@gmail.com>
2024-10-27 00:18:45 +0200

    Extract function download_wbt()

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,18 @@ import whitebox
 WBT_PATH = "WhiteboxTools_linux_amd64/WBT"
 WBT_URL = "https://www.whiteboxgeo.com/WBT_Linux/WhiteboxTools_linux_amd64.zip"
 
-if not Path(WBT_PATH).exists():
-    response = requests.get(WBT_URL)
+
+def download_wbt(url):
+    response = requests.get(url)
     with zipfile.ZipFile(io.BytesIO(response.content), "r") as zip_ref:
         zip_ref.extractall()
     exe_path = WBT_PATH + "/whitebox_tools"
     st_result = os.stat(exe_path)
     os.chmod(exe_path, st_result.st_mode | stat.S_IEXEC)
-    
-st.write("Hi there!")
+
+
+if not Path(WBT_PATH).exists():
+    download_wbt(url=WBT_URL)
 
 os.environ["WBT_PATH"] = WBT_PATH
 wbt = whitebox.WhiteboxTools()
